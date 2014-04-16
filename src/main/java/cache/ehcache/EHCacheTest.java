8fde1740bf9b5fec14c456b99107e2c74eefc90e
xuefeng0707 <xuefeng0707@hotmail.com>
2014-04-17 00:21:39 +0800

    add cache

---
+++ src/main/java/cache/ehcache/EHCacheTest.java
@@ -0,0 +1,23 @@
+package cache.ehcache;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+
+public class EHCacheTest {
+
+	public static void main(String[] args) {
+		CacheManager manager = CacheManager.create(Thread.currentThread().getContextClassLoader().getResourceAsStream("cache/ehcache/ehcache.xml"));
+		manager.addCache("hello");
+		
+		Cache cache =  manager.getCache("hello");
+		cache.put(new Element("1", 11));
+		
+		Cache tcache = manager.getCache("sampleTerracottaCache");
+		tcache.put(new Element("2",22));
+		
+		System.out.println(cache.get("1"));
+		System.out.println(tcache.get("2"));
+	}
+
+}
